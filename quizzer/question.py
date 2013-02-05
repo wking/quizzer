31a37086df83562f1c955ca68d4ea50f2a7cb7bd
W. Trevor King <wking@tremily.us>
2013-02-05 10:15:16 -0500

    Add dependency storage (list of question IDs)

--- quizzer/question.py
+++ quizzer/question.py
@@ -1,11 +1,15 @@
 class Question (object):
-    def __init__(self, id=None, prompt=None, answer=None, help=None):
+    def __init__(self, id=None, prompt=None, answer=None, help=None,
+                 dependencies=None):
         if id is None:
             id = prompt
         self.id = id
         self.prompt = prompt
         self.answer = answer
         self.help = help
+        if dependencies is None:
+            dependencies = []
+        self.dependencies = dependencies
 
     def __getstate__(self):
         return {
@@ -13,11 +17,14 @@ class Question (object):
             'prompt': self.prompt,
             'answer': self.answer,
             'help': self.help,
+            'dependencies': self.dependencies,
             }
 
     def __setstate__(self, state):
         if 'id' not in state:
             state['id'] = state.get('prompt', None)
+        if 'dependencies' not in state:
+            state['dependencies'] = []
         self.__dict__.update(state)
 
     def check(self, answer):
